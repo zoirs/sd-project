5cc16a1e8eec1fe32edecd36f8f6767ceecd59e3
Denis <zoirs@ya.ru>
2025-01-11 15:16:35 +0200

    init

---
+++ src/main/java/com/zoirs/sd_project/SdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zoirs.sd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoirs/sd_project/SdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoirs.sd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
